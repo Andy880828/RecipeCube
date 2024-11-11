6e6d9761c21cedb8c0db6a9ffa1b4baf73cc9f1d
Andy Lin <andy0010502@gmail.com>
2024-11-11 13:43:13 +0800

    加上hmr

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,8 @@ import vue from '@vitejs/plugin-vue';
 import fs from 'fs';
 import path from 'path';
 
+let host = 'localhost';
+
 export default defineConfig({
     plugins: [vue()],
     resolve: {
@@ -16,13 +18,14 @@ export default defineConfig({
             scss: { api: 'modern-compiler' },
         },
     },
-    base: '/',  // 這裡保持為根路徑，或者你也可以設定為 /linetest/ 如果需要
+    base: '/', // 這裡保持為根路徑，或者你也可以設定為 /linetest/ 如果需要
     server: {
         https: {
-            key: fs.readFileSync(path.resolve(__dirname, 'certs', 'key.pem')),  // 私鑰檔案路徑
-            cert: fs.readFileSync(path.resolve(__dirname, 'certs', 'cert.pem')),  // 憑證檔案路徑
+            key: fs.readFileSync(path.resolve(__dirname, 'certs', 'key.pem')), // 私鑰檔案路徑
+            cert: fs.readFileSync(path.resolve(__dirname, 'certs', 'cert.pem')), // 憑證檔案路徑
         },
-        host: 'localhost',
+        hmr: { host },
+        host: host,
         port: 5173,
     },
 });

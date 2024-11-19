89e18ef25f6f15f43574ab04a8be92109a351a3d
Andy Lin <andy0010502@gmail.com>
2024-11-19 15:44:22 +0800

    有地方忘記刪

--- src/stores/oauth.js
+++ src/stores/oauth.js
@@ -204,7 +204,6 @@ export const useOAuthStore = defineStore('oauth', () => {
         token,
         userData,
         oAuthGemail,
-        initializeStore,
         oAuthFirstSignIn,
         callback,
     };
